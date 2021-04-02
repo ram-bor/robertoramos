6a3a6063b432440453fba2ae6162c18499c689bf
RobertoRamos <robram91@gmail.com>
2021-04-02 02:44:27 -0400

    importing and linking icons

--- website/src/Footer/Footer.js
+++ website/src/Footer/Footer.js
@@ -2,32 +2,32 @@ import React, { Component } from 'react'
 import './Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
+
+var bmcUrl = 'https://www.buymeacoffee.com/vevufove'
+var linkedinUrl = 'https://www.linkedin.com/in/rob-ramos/'
+var githubUrl = "https://github.com/ram-bor"
 
 export default class Footer extends Component {
     render() {
         return (
             <div id="Footer">
                 <div className="footer-bar">
-                    <FontAwesomeIcon icon={faCoffee} />
-                    <li className="footer-item">
-                        <a href="https://www.linkedin.com/in/rob-ramos/" target="_blank"><i className="fa fa-linkedin"
-                            aria-hidden="true"></i></a>
-                    </li>
-                    <li className="footer-item">
-                        <a href="https://github.com/ram-bor" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
-                    </li>
-                    <li className="footer-item">
-                        <a href="mailto:roberto_c_ramos&#64;yahoo.com"><i className="fa fa-envelope-o" aria-hidden="true"></i></a>
-                    </li>
-                    <li className="footer-item">
-                        <a href="resume/Roberto_Ramos_Resume.pdf" target="_blank"><i className="fa fa-file-pdf-o"
-                            aria-hidden="true"></i>
-                        </a>
-                    </li>
-                    {/* <li className="footer-item">
-                        <p>Copyright &copy; Roberto Ramos 2020</p>
-                    </li> */}
+                    <a href={bmcUrl}>
+                        <FontAwesomeIcon icon={faCoffee} style=/>
+                    </a>
+                    <a href={linkedinUrl}>
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                    <a href={githubUrl} >
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                    <a href="mailto:roberto_c_ramos&#64;yahoo.com"><i className="fa fa-envelope-o" aria-hidden="true"></i></a>
+                    <a href="resume/Roberto_Ramos_Resume.pdf" target="_blank"><i className="fa fa-file-pdf-o"
+                        aria-hidden="true"></i>
+                    </a>
+                    <p>Copyright &copy; Roberto Ramos 2020</p>
                 </div>
             </div >
         )
