0d01ad16a666e5739c9ba417b6419000cfea3001
RobertoRamos <robram91@gmail.com>
2021-04-02 03:14:23 -0400

    halfway there

--- website/src/Footer/Footer.js
+++ website/src/Footer/Footer.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
-import './Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faMugHot, faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import './Footer.css'
 
 
 var bmcUrl = 'https://www.buymeacoffee.com/vevufove'
 var linkedinUrl = 'https://www.linkedin.com/in/rob-ramos/'
 var githubUrl = "https://github.com/ram-bor"
+var email = "mailto:roberto_c_ramos&#64;yahoo.com"
+var resume = 'https://docs.google.com/document/d/1ekheMuFwPe3dN593Yc3okQ5szLsSbxNG85f9WgI848s/edit'
 
 export default class Footer extends Component {
     render() {
@@ -15,20 +17,37 @@ export default class Footer extends Component {
             <div id="Footer">
                 <div className="footer-bar">
                     <a href={bmcUrl}>
-                        <FontAwesomeIcon icon={faCoffee} style=/>
+                        <FontAwesomeIcon icon={faMugHot} style={{
+                            height: '25px',
+                            width: '25px'
+                        }} />
                     </a>
                     <a href={linkedinUrl}>
-                        <FontAwesomeIcon icon={faLinkedin} />
+                        <FontAwesomeIcon icon={faLinkedin} style={{
+                            height: '25px',
+                            width: '25px'
+                        }} />
                     </a>
                     <a href={githubUrl} >
-                        <FontAwesomeIcon icon={faGithub} />
+                        <FontAwesomeIcon icon={faGithub} style={{
+                            height: '25px',
+                            width: '25px'
+                        }} />
                     </a>
-                    <a href="mailto:roberto_c_ramos&#64;yahoo.com"><i className="fa fa-envelope-o" aria-hidden="true"></i></a>
-                    <a href="resume/Roberto_Ramos_Resume.pdf" target="_blank"><i className="fa fa-file-pdf-o"
-                        aria-hidden="true"></i>
-                    </a>
-                    <p>Copyright &copy; Roberto Ramos 2020</p>
+                    {/* <a href={email}>
+                        <FontAwesomeIcon icon={faEnvelope} style={{
+                            height: '25px',
+                            width: '25px'
+                        }} />
+                    </a> */}
+                    {/* <a href={resume}>
+                        <FontAwesomeIcon icon={faFile} style={{
+                            height: '25px',
+                            width: '25px'
+                        }} />
+                    </a> */}
                 </div>
+                <p className="copyright">Copyright &copy; Roberto Ramos 2020. Revised and revitalized for 2021</p>
             </div >
         )
     }
