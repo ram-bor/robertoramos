d6abb0cec82f5a97d46412852275622f39a70109
RobertoRamos <robram91@gmail.com>
2021-04-02 01:00:58 -0400

    deleted pics

--- website/src/About/About.js
+++ website/src/About/About.js
@@ -7,7 +7,8 @@ export default class About extends Component {
         return (
             <div id="About">
                 <h1>About Me</h1>
-                <LoremIpsum />
+                <p className="about-statement"> I am an intuitive and design-driven full stack developer with years of experience in a wide array of disciplines.</p>
+                <p class="about-text">I learn, I write, I design, I code. </p>My passion for technology and social change allows me to listen with compassion and act with conviction. <br> In my freetime, I enjoy biking, hiking, collecting vinyl, and more recently (due to Covid-19), attempting to cook new and different cuisines at home. <br> I live by the <i>code</i> that in order to improve you have to fail often.</p>
             </div>
         )
     }
