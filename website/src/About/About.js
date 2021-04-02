b5acc337a57e056f382c0bd38c2dffd43c81304b
RobertoRamos <robram91@gmail.com>
2021-04-02 01:30:13 -0400

    cleaning up

--- website/src/About/About.js
+++ website/src/About/About.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react'
 import './About.css'
+import Headshot from './Images/Headshot.jpg'
 
 export default class About extends Component {
     render() {
         return (
             <div id="About">
                 <h1>About Me</h1>
-                <p className="about-statement"> I am an intuitive and design-driven full stack developer with years of experience in a wide array of disciplines.</p>
-                <div class="about-text">
-                    <p>
-                        I learn, I write, I design, I code.
+                <div>
+                    <img src={Headshot} style={{ width: '200px' }} />
+                </div>
+                <div>
+
+                    <p className="about-statement"> I am an intuitive and design-driven software developer with years of experience in a wide array of industries.</p>
+                    <div class="about-text">
+                        <p>
+                            I learn, I write, I design, I code.
                     </p>
-                    <p>
-                        My passion for technology and social change allows me to listen with compassion and act with conviction.
+                        <p>
+                            My passion for technology and social change allows me to listen with compassion and act with conviction.
                     </p>
-                    <p>
-                        In my freetime, I enjoy biking, hiking, collecting vinyl, and recently (due to Covid-19), attempting to cook new and delicious recipes at home.
+                        <p>
+                            In my freetime, I enjoy biking, hiking, collecting vinyl, and recently (due to Covid-19), attempting to cook new and delicious recipes at home.
                     </p>
-                    <p>
-                        I live by the <i>code</i> that in order to improve you have to fail often.
+                        <p>
+                            I live by the <i>code</i> that in order to improve you have to fail often.
                     </p>
+                    </div>
                 </div>
             </div>
         )
