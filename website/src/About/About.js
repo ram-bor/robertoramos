72cea98e1e3feb333b15cdd1b63759bb84f92f09
RobertoRamos <robram91@gmail.com>
2021-04-05 15:28:05 -0400

    working on about me

--- website/src/About/About.js
+++ website/src/About/About.js
@@ -12,23 +12,14 @@ export default class About extends Component {
                 </div>
                 <div>
 
-                    <p className="about-statement"> I am an intuitive and design-driven software developer with years of experience in a wide array of industries.</p>
-                    <div class="about-text">
-                        <p>
-                            I learn, I write, I design, I code.
+                    <p className="about-statement"> I am an intuitive and design-driven software developer with years of experience in a wide array of industries.
+                    I learn, I write, I design, I code.
+                    My passion for technology and social change allows me to listen with compassion and act with conviction.
+                    In my freetime, I enjoy biking, hiking, collecting vinyl, and recently (due to Covid-19), attempting to cook new and delicious recipes at home.
+                        I live by the <i>code</i> that in order to improve you have to fail often.
                     </p>
-                        <p>
-                            My passion for technology and social change allows me to listen with compassion and act with conviction.
-                    </p>
-                        <p>
-                            In my freetime, I enjoy biking, hiking, collecting vinyl, and recently (due to Covid-19), attempting to cook new and delicious recipes at home.
-                    </p>
-                        <p>
-                            I live by the <i>code</i> that in order to improve you have to fail often.
-                    </p>
-                    </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
