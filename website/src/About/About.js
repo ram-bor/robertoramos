683b82ab44583106708686de2c6c872ad66121d3
RobertoRamos <robram91@gmail.com>
2021-04-05 16:18:54 -0400

    finishing up about section basics

--- website/src/About/About.js
+++ website/src/About/About.js
@@ -5,19 +5,20 @@ import Headshot from './Images/Headshot.jpg'
 export default class About extends Component {
     render() {
         return (
-            <div id="About">
+            <div className="about">
                 <h1>About Me</h1>
-                <div>
-                    <img src={Headshot} style={{ width: '200px' }} />
-                </div>
-                <div>
-
-                    <p className="about-statement"> I am an intuitive and design-driven software developer with years of experience in a wide array of industries.
-                    I learn, I write, I design, I code.
-                    My passion for technology and social change allows me to listen with compassion and act with conviction.
-                    In my freetime, I enjoy biking, hiking, collecting vinyl, and recently (due to Covid-19), attempting to cook new and delicious recipes at home.
+                <div className="about-parent">
+                    <div className="image-div">
+                        <img src={Headshot} style={{ width: '300px' }} alt="headshot" className="image" />
+                    </div>
+                    <div className="statement-div">
+                        <p>
+                            As a Software Developer, I seek first to establish a clear understanding of the problem. My resources are
+                            icnw In my freetime, I enjoy biking, hiking, collecting vinyl, and recently (due to Covid-19),
+                            attempting to cook new and delicious recipes at home.
                         I live by the <i>code</i> that in order to improve you have to fail often.
                     </p>
+                    </div>
                 </div>
             </div >
         )
