b470059b441a207493f157ba5c75cfb42959d55f
RobertoRamos <robram91@gmail.com>
2021-04-05 16:31:37 -0400

    working on project section

--- website/src/Portfolio/Portfolio.js
+++ website/src/Portfolio/Portfolio.js
@@ -5,7 +5,12 @@ export default class Portfolio extends Component {
     render() {
         return (
             <div id="Portfolio">
-                <h1>Portfolio</h1>
+                <h1>Projects</h1>
+                <div className="project-parent-container">
+                    <div className="project-div"></div>
+                    <div className="project-div"></div>
+                    <div className="project-div"></div>
+                </div>
             </div>
         )
     }
