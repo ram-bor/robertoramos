6f21d731c33c9ded2c8042721b0e9672a134cb5a
RobertoRamos <robram91@gmail.com>
2021-04-05 16:43:31 -0400

    adding images

--- website/src/Portfolio/Portfolio.js
+++ website/src/Portfolio/Portfolio.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import './Portfolio.css'
+// import Nav from 'react-bootstrap/Nav'
+
+import OrderPorter from './Images/mern-project.png'
+// import NavLink from 'react-bootstrap/esm/NavLink'
 
 export default class Portfolio extends Component {
     render() {
@@ -7,7 +11,9 @@ export default class Portfolio extends Component {
             <div id="Portfolio">
                 <h1>Projects</h1>
                 <div className="project-parent-container">
-                    <div className="project-div"></div>
+                    <div className="project-div">
+                        <img href={OrderPorter} alt="order porter project" style={{ width: '200px' }}></img>Order Porter
+                    </div>
                     <div className="project-div"></div>
                     <div className="project-div"></div>
                 </div>
